c9a7b55bc0742e720066b93f3f399ec4393138e1
sumantagethub <janasumanta721152@gmail.com>
2026-08-30 14:28:11 +0530

    added

--- root/Dev/provider.tf
+++ root/Dev/provider.tf
@@ -9,7 +9,7 @@ backend "azurerm"{
     resource_group_name = "Dev_rg"
     storage_account_name="wiprobackendstate"
     container_name="terraformbackend"
-    key="backend.tfvars"
+    key="backend.tfstate"
 
 
 }
